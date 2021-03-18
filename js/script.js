e091f2ccb209885b5cbf49d06001c15706e7c7b2
francescapetra <francescalaurapetra@gmail.com>
2021-03-18 15:18:11 +0100

    array+ciclo e template

--- js/script.js
+++ js/script.js
@@ -1,9 +1,23 @@
 // Creare una todo list che permetta di aggiungere (tramite campo input) e rimuovere gli elementi!
 
+// creo l'array con un lista già esistente
+var list = [
+  "Fare spesa",
+  "Innaffiare giardino",
+  "Lavare l'auto",
+];
 
+//faccio ciclare i miei elementi
+for (var i = 0; i < list.length; i++) {
+// clono il template con i li
+var template = $(".template ul li").clone();
+// inserisco i miei elementi singoli nel template con prepand
+template.prepend(list[i]);
+//aggiungo il template alla lista esistente to_do_list
+$(".to_do_list").append(template);
+};
 
-
-
+// creo un click generico in previsioni del push successivo da input
 
 
 
