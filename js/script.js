15f896d1013e874b32a7535c9d8b2404b162af88
francescapetra <francescalaurapetra@gmail.com>
2021-03-18 14:57:18 +0100

    creazione cartelle

---
+++ js/script.js
@@ -0,0 +1,54 @@
+// Creare una todo list che permetta di aggiungere (tramite campo input) e rimuovere gli elementi!
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// clicco su domanda e apre e chiude
+// $("div").click(
+//   function(){
+//     $(this).children().toggle();
+//     console.log($(this).children("p"));
+//   }
+// )
+//
+// $("li").click(function () {
+//      $(this).css("color", "orange");
+//      console.log($(this));
+//  });
